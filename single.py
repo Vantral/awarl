687964190c4edca7b535bb47e154a982ed265108
Vantral <42929116+Vantral@users.noreply.github.com>
2019-04-26 13:19:51 +0300

    Update single.py

--- single.py
+++ single.py
@@ -1,17 +1,16 @@
-def nounp():
-    base = '(?:(?:(?:<[^>]+?\sAV0>)?(?:<[^>]+?\s(?:[DA][TP].|POS)>)?(?:<[^>]+?\sAV0>)?' +\
-           '(?:<[^>]+?\s[DA]T.>)?(?:<[^>]+?\s.RD>)?(?:<[^>]+?\sAJ.>)?)*'
-    #facultative attributes of NOUN
-    dnoun = '(?:<[^>]+?\sN..>' + base + '<[^>]+?\sN..>))'
-    # such as "college student"
-    noun_phrase1 = base + '(?:<[^>]+?\s(?:N..|PN.)>))'
-    #such as "actually the best extremely poor student"
-    noun_phrase2 = base + dnoun + ')'
-    #base and "college student"
-    noun_phrase3 = base + '(?:' + dnoun + '|' + '(?:<[^>]+?\s(?:N..|PN.)>))' + '<[^>]+\sPRF>' +\
-                   '(?:' + dnoun + '|' + '(?:<[^>]+?\s(?:N..|PN.)>)))'
-    #constructions with "of" such as "a perfect piece of cake"
-    noun_phrase10 = '(?:<[^>]+?>){0,4}(?:<[^>]+?\s(?:N..|PN.)>)'
-    noun_phrase =  '(?:' + noun_phrase2 + '|' + noun_phrase1 + '|' + noun_phrase3 + ')'
-    #print(noun_phrase)
-    return noun_phrase
+def new_search(pattern, directory = 'C:/Users/Prestigio/Desktop/awarl/new/'):
+    errors = []
+    folders = os.listdir(directory)
+    for folder in folders:
+        print(folder)
+        files = os.listdir(directory + folder)
+        for file in files:
+            text = open_file(directory + folder + '/' + file)
+            sentences = text.split('@')
+            for sent in sentences:
+                if '?' in sent:
+                    a = re.search(pattern, sent, flags=re.IGNORECASE)       
+                    if a:
+                        if len(a.groups()[0].split('><')) > 1:
+                            errors.append([re.sub('^\n', '', sent), file, folder])
+    return errors
